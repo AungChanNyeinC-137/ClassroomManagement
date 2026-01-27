af892fab208da9ec1fc564f89ecd0b3c84331f79
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2026-01-27 20:42:11 +0630

    show middleware error response

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,8 +1,22 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 if(!BACKEND_BASE_URL) {
   throw new Error('BACKEND_BASE_URL is not configured');
+}
+const buildHttpError = async (response: Response):Promise<HttpError> => {
+  let message = 'Request Failed.';
+  try {
+    const payload = (await response.json() as {message?: string})
+    if(payload.message) message = payload.message;
+  } catch  {
+    //Ignore errors
+  }
+  return {
+    message,
+    statusCode : response.status,
+  }
 }
 const options: CreateDataProviderOptions = {
   getList: {
@@ -22,11 +36,13 @@ const options: CreateDataProviderOptions = {
       return params;
     },
     mapResponse: async (response) => {
+      if(!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
+      if(!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     },
